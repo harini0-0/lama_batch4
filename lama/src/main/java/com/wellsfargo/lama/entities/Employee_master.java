8d18ccd9d1747ff432092d0f08ef8053c758aa1d
Prasann7676 <prasanngupta7676@gmail.com>
2023-08-10 22:52:00 +0530

    Added employee_master entity

---
+++ lama/src/main/java/com/wellsfargo/lama/entities/Employee_master.java
@@ -0,0 +1,49 @@
+package com.wellsfargo.lama.entities;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "employee_master")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee_master {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(nullable = false)
+	private String employee_id;
+	
+	@Column(nullable = false, length = 20)
+	private String employee_name;
+	
+	@Column(nullable = false, length = 25)
+	private String designation;
+
+	@Column(nullable = false, length = 25)
+	private String department;
+	
+	@Column(nullable = false)
+	private char gender;
+	
+	@Column(nullable = false)
+	private Date date_of_birth;
+	
+	@Column(nullable = false)
+	private Date date_of_joining;
+	
+}
