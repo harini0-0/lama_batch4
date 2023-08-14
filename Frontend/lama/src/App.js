39fc9a9580155be3efcc0f2442c8902ce098282e
sameera <tazeen.sameera@gmail.com>
2023-08-14 12:33:47 +0530

    commit

--- Frontend/lama/src/App.js
+++ Frontend/lama/src/App.js
@@ -2,12 +2,12 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home';
 import AdminPage from './Pages/AdminLandingPage';
-
+import EmployeePage from './Pages/EmployeeLandingPage';
 function App() {
   return (
     <BrowserRouter>
   <Routes>
-    <Route path="/" element={<AdminPage />} />
+    <Route path="/" element={<Home />} />
     <Route path="/adminpage" element={<AdminPage />} />
     {/* <Route render={() => <Navigate to="/" />} /> */}
   </Routes>
---
+++ Frontend/lama/src/Pages/EmployeeLandingPage.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import NavbarComponent from "./Navbar";
+import { Button } from "react-bootstrap";
+
+function EmployeePage(){
+    return (
+        <div className="App">
+            <NavbarComponent></NavbarComponent>
+            <div className="employeeMenu">
+                <h1>Employee Dashboard</h1>
+                <div className="employeeOptions">
+                    <Button variant="secondary" style={{margin: "10px"}}>View Loans</Button>
+                    <Button variant="secondary" style={{margin: "10px"}}>Apply for loan</Button>
+                    <Button variant="secondary" style={{margin: "10px"}}>View Items Purchased</Button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default AdminPage;
