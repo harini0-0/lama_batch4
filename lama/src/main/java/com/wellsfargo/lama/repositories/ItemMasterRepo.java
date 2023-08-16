80ae9949f347dc75df8bd91631e1546aff39578d
prasann7676 <prasanngupta7676@gmail.com>
2023-08-16 16:35:53 +0530

    Added exceptions in item master

--- lama/src/main/java/com/wellsfargo/lama/repositories/ItemMasterRepo.java
+++ lama/src/main/java/com/wellsfargo/lama/repositories/ItemMasterRepo.java
@@ -1,9 +1,12 @@
 package com.wellsfargo.lama.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.wellsfargo.lama.entities.ItemMaster;
 
 public interface ItemMasterRepo extends JpaRepository<ItemMaster, Integer> {
-
+	
+	Optional<ItemMaster> findByItemId(int itemId);
 }
