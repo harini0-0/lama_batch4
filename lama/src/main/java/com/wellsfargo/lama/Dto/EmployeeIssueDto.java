ca6d866c1daf8d61bc06e8d9c2acc5ed59ce92b1
rajatrathi31 <rajath3110@gmail.com>
2023-08-16 13:46:37 +0530

    Added config file

---
+++ lama/src/main/java/com/wellsfargo/lama/Dto/EmployeeIssueDto.java
@@ -0,0 +1,12 @@
+package com.wellsfargo.lama.Dto;
+
+import java.sql.Date;
+
+public class EmployeeIssueDto {
+	private int issueId;
+	private int employeeId;
+	private int itemId;
+	private Date issueDate;
+	private Date returnDate;
+	private int isApproved;
+}
