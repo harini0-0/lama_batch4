c141763c849bcfc6428f5f3ad8948a5d566e83aa
rajatrathi31 <rajath3110@gmail.com>
2023-08-28 12:04:24 +0530

    Dashboard Controller Added

---
+++ lama/src/main/java/com/wellsfargo/lama/controllers/DashboardController.java
@@ -0,0 +1,31 @@
+package com.wellsfargo.lama.controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.wellsfargo.lama.services.EmployeeIntegrationService;
+
+@RestController
+@RequestMapping("api/v1/dashboard")
+public class DashboardController {
+	@Autowired
+	private EmployeeIntegrationService employeeIntegrationService;
+	
+	@GetMapping("/")
+	public ResponseEntity<List<Integer>> getAllRequests() {
+		Integer AllRequests = employeeIntegrationService.allRequests();
+		Integer approvedRequests = employeeIntegrationService.approvedRequests();
+		
+		List<Integer> dashboardRequests= new ArrayList<Integer>();
+		dashboardRequests.add(approvedRequests);
+		dashboardRequests.add(AllRequests);
+		return new ResponseEntity<List<Integer>>(dashboardRequests, HttpStatus.OK);
+	}
+}
