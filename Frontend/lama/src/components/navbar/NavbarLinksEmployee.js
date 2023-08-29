ac0db7e726ad7df1c31a433a7620a80e88f0dec4
harini0-0 <harinipri2001@gmail.com>
2023-08-29 12:11:37 +0530

    profile name

--- Frontend/lama/src/components/navbar/NavbarLinksEmployee.js
+++ Frontend/lama/src/components/navbar/NavbarLinksEmployee.js
@@ -192,7 +192,7 @@ export default function HeaderLinks(props) {
 					<Avatar
 						_hover={{ cursor: 'pointer' }}
 						color="white"
-						name="Adela Parkson"
+						name="Employee"
 						bg="#11047A"
 						size="sm"
 						w="40px"
