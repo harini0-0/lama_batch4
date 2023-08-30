1ebd3ff795d5c014bb9d033b073538923dc50dbf
harini0-0 <harinipri2001@gmail.com>
2023-08-30 13:38:00 +0530

    cod ecleaned 30%

--- Frontend/lama/src/App.js
+++ Frontend/lama/src/App.js
@@ -1,38 +1,21 @@
-// import './App.css';
-// import { BrowserRouter, Route, Routes } from 'react-router-dom';
-// import Home from './Pages/Home';
-// import AdminPage from './Pages/AdminLandingPage';
 import CustomerMasterPage from './Pages/customerMasterPage';
 import LoanMasterPage from './Pages/loanMasterPage';
 import PendingLoansAdmin from './Pages/PendingLoansAdmin';
 import ItemMasterPage from './Pages/itemMasterPage';
-// import LoginPage from './Pages/Login';
 import AdminLayout from './layouts/admin/index.js';
 import EmployeeLayout from './layouts/employee/index.js'
 import AuthLayout from "./layouts/auth/index.js";
 import RtlLayout from "./layouts/rtl/index.js"
-// import EmployeePage from './Pages/EmployeeLandingPage';
-import React, { useEffect } from 'react';
-// import ReactDOM from 'react-dom';
+import React from 'react';
 import './assets/css/App.css';
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
-// import AuthLayout from 'layouts/auth';
-// import AdminLayout from 'layouts/admin';
-// import RtlLayout from 'layouts/rtl';
-import { ChakraProvider } from '@chakra-ui/react';
-// import theme from 'theme/theme';
 import AddCustomerComponent from './Pages/AddCustomerComponent';
 import UpdateCustomerComponent from './Pages/UpdateCustomerComponent';
-
 import AddLoanComponent from './Pages/AddLoanComponent';
 import UpdateLoanComponent from './Pages/UpdateLoanComponent';
-
-import { ThemeEditorProvider } from '@hypertheme-editor/chakra-ui';
 import { isLoggedIn } from './auth';
-
 import ApplyForLoan from './Pages/ApplyForLoan';
 import ProtectedRoute from './services/ProtectedRotue';
-// import { BrowserRouter } from 'react-router-dom';
 import ItemsPage from './Pages/ItemsPage';
 import EmployeeItemPage from './Pages/EmployeeItemPage'
 import ProtectedRouteUser from './services/ProtectedRouteUser';
@@ -43,37 +26,25 @@ import LoansViewAdmin from './Pages/LoansViewPage';
 
 function App() {
 
- const loggedIn = isLoggedIn()	
- const userToken = JSON.parse(localStorage.getItem("data"));
   return (
 	<BrowserRouter>
 		<Switch>
-			<Route path={`/auth`} component={AuthLayout} />
-
-			
-			{/* <Route path={`/apply`} component={Apply} /> */}
- 			
+			<Route path={`/auth`} component={AuthLayout} /> 			
 			<ProtectedRouteUser path={`/employee`} component={EmployeeLayout} />
-			
-
-			<ProtectedRoute path={`/admin`} component={AdminLayout} />
 			<ProtectedRouteUser path={'/itemsPage'} component={ItemsPage} />
+			<ProtectedRouteUser path = {'/user/item/:id'} component = {EmployeeItemPage}/>
+			<ProtectedRouteUser path="/viewloan" component={EmployeeLoanPage} />
+			<ProtectedRouteUser path={`/loan/apply`} component={ApplyForLoan} />
+			<ProtectedRoute path={`/admin`} component={AdminLayout} />
 			<ProtectedRoute path={`/rtl`} component={RtlLayout} />
 			<ProtectedRoute path={`/customermap`} component={CustomerMasterPage} />
 			<ProtectedRoute path={'/loanviewmap'} component={LoansViewAdmin} />
-
-			<ProtectedRouteUser path = {'/user/item/:id'} component = {EmployeeItemPage}/>
-
 			<ProtectedRoute path={`/user/add`} component={AddCustomerComponent} />
 			<ProtectedRoute path={'/pendingLoans'} component={PendingLoansAdmin}/>
 			<ProtectedRoute path="/user/:id" component={UpdateCustomerComponent } />
-
 			<ProtectedRoute path={`/loanmap`} component={LoanMasterPage } />
-			<ProtectedRouteUser path={`/loan/apply`} component={ApplyForLoan} />
 			<ProtectedRoute path={`/loan/add`} component={AddLoanComponent } />
-			<ProtectedRoute path="/loan/:id" component={UpdateLoanComponent } />
-			<ProtectedRouteUser path="/viewloan" component={EmployeeLoanPage} />
-			
+			<ProtectedRoute path="/loan/:id" component={UpdateLoanComponent } />	
 			<ProtectedRoute path={`/itemmap`} component={ItemMasterPage} />
 			<ProtectedRoute path={`/item/add`} component={AddItemMaster} />
 			<ProtectedRoute path="/item/:id" component={UpdateItemMasterPage} />
@@ -88,14 +59,3 @@ function App() {
 
 export default App;
 
-{/* //     <BrowserRouter>
-//   <Routes>
-//     <Route path="/" element={<AdminLayout />} />
-//     <Route path="/adminpage" element={<AdminPage />} />
-//     <Route path="/customermap" element={<CustomerMasterPage />} />
-//     <Route path="/login" element={<LoginPage/>} />
-//     <Route path="/admin" element={<AdminLayout />} />
-//     <Route path="/auth" element={<AuthLayout />} />
-//     <Route render={() => <Navigate to="/" />} />
-//   </Routes>
-// </BrowserRouter> */}
